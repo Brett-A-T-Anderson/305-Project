52bc6ae7f2e5ca46febec8f39ce343225b5d0bf0
JackStaples <staplesj3@mymacewan.ca>
2016-11-16 11:50:28 -0700

    Add files via upload
    Querying should work now, Url_test contains the testing for the queries currently (uncomment it out).

--- Url_test.java
+++ Url_test.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkg305_project;
 
 
 import java.net.*;
@@ -11,134 +5,202 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.System.currentTimeMillis;
+
 public class Url_test {
-    
+
     public static void main(String[] args) throws Exception {
-        
-        
-        
-        Laureates Lauerates = new Laureates();
+
+       /* Laureate laur1 = new Laureate();
+        Laureate laur2 = new Laureate();
+        Laureate laur3 = new Laureate();
+        Laureate laur4 = new Laureate();
+        Laureate laur5 = new Laureate();
+        Laureate laur6 = new Laureate();
+
+        laur1.addBirth("1956", "canada", "CA", "edmonton");
+        laur1.addDeath("", "", "", "");
+        laur1.addMisc("1", "don", "keedix", "don keedix", "male");
+        laur1.addPrize("2000", "physics", "for contributions to stuff", "1", "he felt like it, fam", "toronto", "canada");
+
+        laur2.addBirth("1974", "canada", "CA", "toronto");
+        laur2.addDeath("", "", "", "");
+        laur2.addMisc("2", "betty", "phuckter", "betty phuckter", "female");
+        laur2.addPrize("2008", "peace", "for max package 2: the repackaging", "2", "she was just along for the ride", "chicago", "usa");
+
+        laur3.addBirth("1982", "germany", "DE", "berlin");
+        laur3.addDeath("2016", "brazil", "BR", "rio de janero");
+        laur3.addMisc("3", "max", "package", "max package", "male");
+        laur3.addPrize("2008", "peace", "for max package 2: the repackaging", "2", "to share his package with the world", "chicago", "usa");
+
+        laur4.addBirth("1905", "england", "EN", "london");
+        laur4.addDeath("1982", "france", "FR", "paris");
+        laur4.addMisc("4", "hugh", "mungus", "hugh mungus", "male");
+        laur4.addPrize("1983", "chemisty", "he had good chemisty", "1", "insert stupid joke here", "france", "paris");
+
+        laur5.addBirth("1932", "canada", "CA", "edmonton");
+        laur5.addDeath("1982", "france", "FR", "paris");
+        laur5.addMisc("5", "max", "package", "max package","male");
+        laur5.addPrize("1955", "physics", "for contributions to stuff", "1", "he felt like it, fam", "toronto", "canada");
+
+        laur6.addBirth("1954", "england", "EN", "chelsea");
+        laur6.addDeath("", "", "", "");
+        laur6.addMisc("6", "dixxy", "normus", "dixxy normus", "female");
+        laur6.addPrize("1994", "literature", "for contributions to stuff", "1", "she felt like it, fam", "chicago", "usa");
+
+        ArrayList<Laureate> laurs = new ArrayList<Laureate>();
+        laurs.add(laur1);
+        laurs.add(laur2);
+        laurs.add(laur3);
+        laurs.add(laur4);
+        laurs.add(laur5);
+        laurs.add(laur6);
+
+        Database data = new Database(laurs);
+        Query test = new Query("max", "package","","","","","");
+        ArrayList<Laureate> max = data.runQuery(test);
+        for (int i = 0; i < max.size(); i++){
+           System.out.println(max.get(i).getName() +" "+ max.get(i).getYear() +" "+ max.get(i).getCategory());
+        }
+        System.out.println("==================================================max package");
+        test = new Query("", "","","","","male","");
+        max = data.runQuery(test);
+        for (int i = 0; i < max.size(); i++){
+            System.out.println(max.get(i).getName() +" "+ max.get(i).getGender() +" "+ max.get(i).getCategory());
+        }
+        System.out.println("==================================================male");
+        test = new Query("", "","2008","","","","");
+        max = data.runQuery(test);
+        for (int i = 0; i < max.size(); i++){
+            System.out.println(max.get(i).getName() +" "+ max.get(i).getYear() +" "+ max.get(i).getCategory());
+        }
+        System.out.println("==================================================2008");
+        test = new Query("", "","1905","2007","","","");
+        max = data.runQuery(test);
+        for (int i = 0; i < max.size(); i++){
+            System.out.println(max.get(i).getName() +" "+ max.get(i).getYear() +" "+ max.get(i).getCategory());
+        }
+        System.out.println("==================================================1905-2007")
+        test = new Query("", "","","","","","canada");
+        max = data.runQuery(test);
+        for (int i = 0; i < max.size(); i++){
+            System.out.println(max.get(i).getName() +" "+ max.get(i).getYear() +" "+ max.get(i).getCategory());
+        }
+        System.out.println("==================================================canada");
+        test = new Query("", "","","","physics","","");
+        max = data.runQuery(test);
+        for (int i = 0; i < max.size(); i++){
+            System.out.println(max.get(i).getName() +" "+ max.get(i).getYear() +" "+ max.get(i).getCategory());
+        }
+        System.out.println("==================================================physics");
+
+
+        System.out.println("Completed running, array len: " + laurs.size());*/
+
+       // END OF QUERY TESTING
+
+
+        /*Laureates Lauerates = new Laureates();
         Scanner fileIn = new Scanner(System.in);
-        String laurURl = laureates(fileIn);
-	String prizeURl = prizes(fileIn);	       
         fileIn.close();
-                
 
-        //System.out.println(laurURl);
-                
-	URL csv = new URL(laurURl);
-	BufferedReader in = new BufferedReader(new InputStreamReader(csv.openStream()));
-	String inputLine;
-	while((inputLine = in.readLine()) != null) {
-            //System.out.println(inputLine);
+        URL csv = new URL("http://api.nobelprize.org/v1/laureate.csv");
+        BufferedReader in = new BufferedReader(new InputStreamReader(csv.openStream()));
+        String inputLine;
+        while((inputLine = in.readLine()) != null) {
             Lauerates.addTo(inputLine);
-                
         }
-        
-        URL oracle = new URL(prizeURl);
-        BufferedReader Prizes = new BufferedReader(
-                new InputStreamReader(oracle.openStream()));
-        List<String> hold = new ArrayList<>();
-        String PrizeLine;
-        while ((PrizeLine = Prizes.readLine()) != null) {
-            hold.add(PrizeLine);
-        }
-        Prizes.close();
-        
-        
-        for (int i = 0; i < hold.size(); i++){
-            System.out.println(hold.get(i));
-        }        
-        
+        in.close();*/
+    }
 
-}
-        
-    private static String laureates(Scanner fileIn) {
-        
+    /*private static String laureates(Scanner fileIn) {
+
         //Scanner fileIn = new Scanner(System.in);
-	String laurURL = "http://api.nobelprize.org/v1/laureate.csv?";
-	System.out.println("What ID would you like to look for");
-	String id = fileIn.nextLine();	
-	System.out.println("What Country was the laureate born in");
-	String bornCountry = fileIn.nextLine();
-	System.out.println("What is the country code of the place they were born in");
-	String bornCountryCode = fileIn.nextLine();
-	System.out.println("What City were they born in?");
-	String bornCity = fileIn.nextLine();
-	System.out.println("What city did they die in?");
-	String diedCountry = fileIn.nextLine();
-	System.out.println("Country code for death location?");
-	String diedCountryCode = fileIn.nextLine();
-	System.out.println("What city did they die in");
-	String diedCity = fileIn.nextLine();
-	System.out.println("What date were they born, either YYYY or YYYYMMDD");
-	String bornDate = fileIn.nextLine();
-	System.out.println("What year would you like to look from for born date?");
-	String bornDateTo = fileIn.nextLine();
-	System.out.println("What year did they die?");
-	String diedDate = fileIn.nextLine();
-	System.out.println("What year did they die to?");
-	String diedDateTo = fileIn.nextLine();
-	System.out.println("Gender?");
-	String gender = fileIn.nextLine();
-		
-	if (!id.equals("")){
+        String laurURL = "http://api.nobelprize.org/v1/laureate.csv?";
+        System.out.println("What ID would you like to look for");
+        String id = fileIn.nextLine();
+        System.out.println("What Country was the laureate born in");
+        String bornCountry = fileIn.nextLine();
+        System.out.println("What is the country code of the place they were born in");
+        String bornCountryCode = fileIn.nextLine();
+        System.out.println("What City were they born in?");
+        String bornCity = fileIn.nextLine();
+        System.out.println("What city did they die in?");
+        String diedCountry = fileIn.nextLine();
+        System.out.println("Country code for death location?");
+        String diedCountryCode = fileIn.nextLine();
+        System.out.println("What city did they die in");
+        String diedCity = fileIn.nextLine();
+        System.out.println("What date were they born, either YYYY or YYYYMMDD");
+        String bornDate = fileIn.nextLine();
+        System.out.println("What year would you like to look from for born date?");
+        String bornDateTo = fileIn.nextLine();
+        System.out.println("What year did they die?");
+        String diedDate = fileIn.nextLine();
+        System.out.println("What year did they die to?");
+        String diedDateTo = fileIn.nextLine();
+        System.out.println("Gender?");
+        String gender = fileIn.nextLine();
+
+        if (!id.equals("")){
             String laurID = "&id=" + id;
             laurURL = laurURL + laurID;
-	}
-	if (!bornCountry.equals("")){
+        }
+        if (!bornCountry.equals("")){
             String laurBornCountry = "&bornCountry=" + bornCountry;
             laurURL = laurURL + laurBornCountry;
-	}
-	if (!bornCountryCode.equals("")){
+        }
+        if (!bornCountryCode.equals("")){
             String laurBornCountryCode = "&bornCountryCode=" + bornCountryCode;
             laurURL = laurURL + laurBornCountryCode;
-	}
-	if (!bornCity.equals("")){
-	String laurBornCity = "&bornCity=" + bornCity ;
-	laurURL = laurURL + laurBornCity;
-	}
-	if (!diedCountry.equals("")){
-	String laurDiedCountry = "&diedCountry=" + diedCountry;
-	laurURL = laurURL + laurDiedCountry;
-	}
-	if (!diedCountryCode.equals("")){
-	String laurDiedCountryCode = "&diedCountryCode=" + diedCountryCode;
-	laurURL = laurURL + laurDiedCountryCode;
-	}
-	if (!diedCity.equals("")){
-	String laurDiedCity = "&diedCity=" + diedCity;
-	laurURL = laurURL + laurDiedCity;
-	}
-	if (!bornDate.equals("")){
-	String laurBornDate = "&bornCity=" + bornDate;
-	laurURL = laurURL + laurBornDate;
-	}
-	if (!bornDateTo.equals("")){
-	String laurBornDateTo = "&bornDateTo=" + bornDateTo;
-	laurURL = laurURL + laurBornDateTo;
-	}
-	if (!diedDate.equals("")){
-	String laurDiedDate = "&diedDate=" + diedDate;
-	laurURL = laurURL + laurDiedDate;
-	}
-	if (!diedDateTo.equals("")){
-	String laurDiedDateTo = "&diedDateTo=" + diedDateTo;
-	laurURL = laurURL + laurDiedDateTo;
-	}
-	if (!gender.equals("")){
-	String laurGender = "&gender=" + gender;
-	laurURL = laurURL + laurGender;
-	} 
+        }
+        if (!bornCity.equals("")){
+            String laurBornCity = "&bornCity=" + bornCity ;
+            laurURL = laurURL + laurBornCity;
+        }
+        if (!diedCountry.equals("")){
+            String laurDiedCountry = "&diedCountry=" + diedCountry;
+            laurURL = laurURL + laurDiedCountry;
+        }
+        if (!diedCountryCode.equals("")){
+            String laurDiedCountryCode = "&diedCountryCode=" + diedCountryCode;
+            laurURL = laurURL + laurDiedCountryCode;
+        }
+        if (!diedCity.equals("")){
+            String laurDiedCity = "&diedCity=" + diedCity;
+            laurURL = laurURL + laurDiedCity;
+        }
+        if (!bornDate.equals("")){
+            String laurBornDate = "&bornCity=" + bornDate;
+            laurURL = laurURL + laurBornDate;
+        }
+        if (!bornDateTo.equals("")){
+            String laurBornDateTo = "&bornDateTo=" + bornDateTo;
+            laurURL = laurURL + laurBornDateTo;
+        }
+        if (!diedDate.equals("")){
+            String laurDiedDate = "&diedDate=" + diedDate;
+            laurURL = laurURL + laurDiedDate;
+        }
+        if (!diedDateTo.equals("")){
+            String laurDiedDateTo = "&diedDateTo=" + diedDateTo;
+            laurURL = laurURL + laurDiedDateTo;
+        }
+        if (!gender.equals("")){
+            String laurGender = "&gender=" + gender;
+            laurURL = laurURL + laurGender;
+        }
         //fileIn.close();
+        System.out.println(laurURL);
         return laurURL;
     }
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
     private static String prizes(Scanner fileIn) {
         //Scanner fileIn = new Scanner(System.in);
         System.out.println("What category would you like to look for?");
@@ -167,6 +229,7 @@ public class Url_test {
             prizeURl = prizeURl + catNumLaurs;
         }
         //fileIn.close();
+        System.out.println(prizeURl);
         return prizeURl;
-    }
+    }*/
 }
