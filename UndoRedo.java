2ac0074f22a6d4a493d362e95a0b90ed7881207e
JackStaples <staplesj3@mymacewan.ca>
2016-12-02 13:09:49 -0700

    Add files via upload

---
+++ UndoRedo.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by stapl on 12/2/2016.
+ */
+public class UndoRedo {
+    private List<Query> Undo;
+    private List<Query> Redo;
+
+    public UndoRedo(){
+        Undo = new ArrayList<Query>();
+        Redo = new ArrayList<Query>();
+    }
+
+    public void addUndo(Query in){
+        Undo.add(in);
+    }
+
+    public void addRead(Query in){
+        Redo.add(in);
+    }
+
+    public Query grabUndo(){
+        Query temp = Undo.get(0);
+        Undo.remove(0);
+        Redo.add(temp);
+        return temp;
+    }
+    public Query grabRedo(){
+        Query temp = Redo.get(0);
+        Redo.remove(0);
+        return temp;
+    }
+
+}
